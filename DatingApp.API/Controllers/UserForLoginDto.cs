b1211f809e5b7bf30681f3aa0b40c4961e21284f
Marius Skauen <mariusskauen@outlook.com>
2018-12-22 13:46:07 +0100

    Added login n' shit

--- DatingApp.API/Controllers/UserForLoginDto.cs
+++ DatingApp.API/Controllers/UserForLoginDto.cs
@@ -1,14 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace DatingApp.API.Dtos
+namespace DatingApp.API.Controllers
 {
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
         [Required]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage= "More than 4 less than 8.")]
         public string Password { get; set; }
     }
 }
